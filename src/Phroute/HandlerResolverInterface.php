05253cfb1ef4665340d28a8e65444bca733b037d
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2018-05-15 12:07:04 +0200

    Minor space formatting

--- src/Phroute/HandlerResolverInterface.php
+++ src/Phroute/HandlerResolverInterface.php
@@ -1,15 +1,15 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace Phroute\Phroute;
 
 interface HandlerResolverInterface {
-	
+
 	/**
 	 * Create an instance of the given handler.
 	 *
 	 * @param $handler
 	 * @return array
 	 */
-	
+
 	public function resolve($handler);
 }
