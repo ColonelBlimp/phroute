e98bd62494624626c356389ac78e0f712c3c9276
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2018-05-17 06:19:35 +0200

    Fixed return type for HandlerResolver and changed composer vendor/package

--- src/Phroute/HandlerResolver.php
+++ src/Phroute/HandlerResolver.php
@@ -8,9 +8,9 @@ class HandlerResolver implements HandlerResolverInterface
      * Create an instance of the given handler.
      *
      * @param $handler
-     * @return array
+     * @return array|object
      */
-    public function resolve($handler): array
+    public function resolve($handler)
     {
         if (\is_array($handler) && \is_string($handler[0])) {
             $handler[0] = new $handler[0];
